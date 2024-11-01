6ce3cc53482b8b94f050549e1b478ebcfab73f1b
ramon-rabello <ramonrabellorj@gmail.com>
2024-11-01 11:22:53 -0300

    initial-commit

---
+++ eitri-app.conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "name": "Eitri llama bot",
+  "title": "Eitri llama bot",
+  "slug": "Eitri-llama-bot",
+  "eitri-luminus": "1.69.2",
+  "eitri-bifrost": "2.9.0",
+  "eitri-commons": "2.1.2",
+  "version": "0.1.0",
+  "public-key": "8e647a12-014d-4b37-a0c1-93b768d32d8d",
+  "applicationId": "4e9c306f-1d20-4baf-b6e7-8a3db5c0601b",
+  "id": "1007bfbd-176e-4618-b166-b91a71bc7918",
+  "organizationId": "bd2951a2-0dc8-4740-b6c2-01a06db0362e",
+  "organization": {}
+}
